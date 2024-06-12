ff56fcc714e4a4463eecd3f3b67e70aee788f694
Nitish <ntsh.vicky@gmail.com>
2024-06-13 02:47:10 +0530

    first commit 3

--- pdf_master/settings.py
+++ pdf_master/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-xgi60%=w3&auq7obevekj)3#qo!3m#prb1wsr$v3j$3$k%wq*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-pdf-editor.vercel.app/']
 
 
 # Application definition
@@ -50,7 +50,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = "pdf_master.urls"
 
@@ -118,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
